34c069726c28c68bc85041ccb7df24be05a0aafc
Sinice1981 <vinakubani@rambler.ru>
2022-09-26 10:44:48 +0300

    added 3

---
+++ task33/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {1,426,739,1,56,9,7,1};
+
+int n = array.Length;
+int find = 1;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+    Console.WriteLine(index);
+    break;//заканчивает поиск на первом элементе
+    }
+    index++;
+}
+
